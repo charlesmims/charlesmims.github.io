f8b8563d70d94e6e192c1d5bc763fad15dc66afd
Charles Mims <charles@mims.io>
2017-09-13 12:33:34 -0500

    update script does it all

--- update.sh
+++ update.sh
@@ -2,5 +2,5 @@ cd ~/blog && hugo
 cd ~/charlesmims.github.io/ &&
 cp -R ~/blog/public/* ~/charlesmims.github.io/. &&
 git add . &&
-git commit -m "${1}"
-# git push
+git commit -m "${1}" &&
+git push
